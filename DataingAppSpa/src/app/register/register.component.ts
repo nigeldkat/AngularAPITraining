132c16c80d218198acea9d68762cc38527c54daf
nigeldkat <nigeldkat@hotmail.com>
2020-07-10 15:54:00 -0400

    using form builder vs new formgroup

--- DataingAppSpa/src/app/register/register.component.ts
+++ DataingAppSpa/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, ɵbypassSanitizationTrustResourceUrl } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
@@ -16,26 +16,41 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private alertService: AlertifyService) { }
+    private alertService: AlertifyService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup(
-      {
-        username: new FormControl('hi', Validators.required),
-        password: new FormControl('', [
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(8)
-        ]),
-        confirmPassword: new FormControl('', Validators.required)
-      },
-      this.passwordMatchValidator
-    );
+    // this.registerForm = new FormGroup(
+    //   {
+    //     username: new FormControl('hi', Validators.required),
+    //     password: new FormControl('', [
+    //       Validators.required,
+    //       Validators.minLength(4),
+    //       Validators.maxLength(8)
+    //     ]),
+    //     confirmPassword: new FormControl('', Validators.required)
+    //   },
+    //   this.passwordMatchValidator
+    // );
+    this.createRegisterForm();
   }
 
   passwordMatchValidator(g: FormGroup) {
     return g.get('password').value
       === g.get('confirmPassword').value ? null : { mismatch: true };
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]],
+      confirmPassword: ['', Validators.required]
+    },
+    {validators: this.passwordMatchValidator});
   }
 
   register() {
